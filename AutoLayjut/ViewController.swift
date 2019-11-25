f6cf55da902f06df2c5d3c3077bb6fe7caf2a258
Valentyn <49493695+Softsinner@users.noreply.github.com>
2019-11-25 21:06:37 +0200

    Initial commit

---
+++ AutoLayjut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayjut
+//
+//  Created by Валентин Гуріненко on 24.11.2019.
+//  Copyright © 2019 Валентин Гуріненко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
